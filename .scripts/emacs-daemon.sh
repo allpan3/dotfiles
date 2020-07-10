a0582445512689de4045ae8f24ff9a6d6097ecfa
Allen Pan <aifusenno1@gmail.com>
2020-07-10 03:54:15 -0700

    Support both GUI and CLI spacemacs with separate servers Tramp support is yet to be implemented

--- .scripts/emacs-daemon.sh
+++ .scripts/emacs-daemon.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-## Used in emacsclient -a option
-# Intend to mimic the default -a "" behavior, but not sure if there's anything missed.
+### Description
+# Used in emacsclient -a option
+# Intend to mimic the default -a "" behavior, but not sure if there's anything
+# missed.
 
-# load ssh agent to avoid entering passphrase when using tramp
-test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh
+# get the server name
+while getopts ":f:" option
+do
+    case "${option}" in
+        f) server_name=${OPTARG};;
+    esac
+done
 
-emacs -daemon
-emacsclient -a "" -nw -q -f ~/.emacs.d/server/server "$@"
+
+emacs --daemon=${server_name}
+
+if [ "${server_name}" = "gui_server" ]; then
+    # load ssh agent to avoid entering passphrase when using tramp
+    test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh
+    emacsclient -c -n $@
+elif [ "${server_name}" = "cli_server" ]; then
+    emacsclient -nw $@
+fi
+
+
+
 
