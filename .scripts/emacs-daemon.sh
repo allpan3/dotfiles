9b582065b93484042070652c35931f906b08e741
Allen Pan <aifusenno1@gmail.com>
2020-07-12 06:16:55 -0700

    Added TRAMP and sudo support to emacs
    ssh scripts are improved to allow controlling whether to add key or not and
    reutrn status
    emacs gui uses tcp server, cli uses UNIX domain socket
    bash alias updated to comform the new emacs.sh

--- .scripts/emacs-daemon.sh
+++ .scripts/emacs-daemon.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
 ### Description
-# Used in emacsclient -a option
-# Intend to mimic the default -a "" behavior, but not sure if there's anything
-# missed.
+# Used in emacsclient -a option to start daemon if server not found.
+# This file should only be executed once, at the server creation time
+# Later on, modify the script to use UNIX domain socket for forwarding instead following instruction here: https://blog.goldandapager.io/a-better-emacs-remote-editing-workflow/
 
 # get the server name
-while getopts ":f:" option
-do
+while getopts ":T:f:s:" option; do
     case "${option}" in
-        f) server_name=${OPTARG};;
+        f|s)
+            server_name=${OPTARG}
+            server_arg="-${option} ${OPTARG}";;
     esac
 done
 
+if [[ "${server_name}" == *"gui_server"* ]]; then
+    # load ssh key to avoid entering passphrase when using tramp
+    # but only load if key is found. We do not want to type passphrase at emacs
+    # startup bascuse we may not use tramp.
+    # will need to type passphrase one time in this case if we use tramp later
+    test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh -d
+    ret=$?
 
-emacs --daemon=${server_name}
-
-if [ "${server_name}" = "gui_server" ]; then
-    # load ssh agent to avoid entering passphrase when using tramp
-    test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh
+    emacs --daemon=${server_name}
+    # give time for server init to complete since it's in the background
+    sleep 0.5
+    # open a frame
     emacsclient -c -n $@
-elif [ "${server_name}" = "cli_server" ]; then
+
+    # copy server file to all remote server if ssh key is found
+    # otherwise, let ssh do the copy when first time entering passphrase
+    if [[ $ret -eq 0 ]]; then
+        scp ~/.emacs.d/server/gui_server allpan@vm:~/.emacs.d/server 2> /dev/null
+    fi
+
+elif [[ "${server_name}" == *"cli_server"* ]]; then
+    emacs --daemon=${server_name}
     emacsclient -nw $@
 fi
 
--- .scripts/emacs.sh
+++ .scripts/emacs.sh
@@ -4,45 +4,62 @@
 # A wrapper for emacsclient; If server is not yet opened, open a server first
 # Inspired by https://andy.wordpress.com/2013/01/03/automatic-emacsclient/
 #
-# emacs.sh -f SERVER_FILE [options]
-# -f is required and must be the first arg
+# emacs.sh -f|s SERVER_FILE [options]
+# -f|s is required. -f arg should just be the server name. -s arg should contain the full path
+# --sudo: open the file with sudo
 ###
 
 
 # get the server name
-while getopts ":f:" option
+while getopts ":f:s:-:" option
 do
     case "${option}" in
-        f) server_name=${OPTARG};;
+        f|s) server_name=${OPTARG}
+             server_arg="-${option} ${OPTARG}";;
+        -) ;; # workaroud, otherwise --sudo will be parsed by -s
     esac
 done
 
-# Checks if there's a frame open
-emacsclient -n -f ${server_name} -e "(if (> (length (frame-list)) 1) 't)" 2> /dev/null | grep t &> /dev/null
 
-############### Notices ##################
+# parse arguments
+params=()
+sudo=""
+
+for p in "$@"
+do
+    if [[ "$p" == "--sudo" ]]; then
+        sudo="-T /sudo:localhost:"
+    else
+        params+=( "$p" )
+    fi
+done
+
+
+# Checks if there's a frame open
+emacsclient -n ${server_arg} -e "(if (> (length (frame-list)) 1) 't)" 2> /dev/null | grep t &> /dev/null
+
 # -n and -nw should not be specified together, while -n and -c can (and should
 # always be in our case)
 # When -a is specified, default will open CLI; specify -c to open GUI
-##########################################
 
 # if no frame open, open one
 if [ "$?" -eq 1 ]; then
-
     # test if server exists. If not, open a daemon and try emacsclient again
     # open a gui server or a cli server based on the first argument
-    if [ "${server_name}" = "gui_server" ]; then
-        emacsclient -a "emacs-daemon.sh $*" -c -n "$@"
-    elif [ "${server_name}" = "cli_server" ]; then
-        emacsclient -a "emacs-daemon.sh $*" -nw -q "$@"
+    if [[ "${server_name}" == *"gui_server"* ]]; then
+        # notice when executing alternate editor, filename is passed in automatically so no beed to pass in -a
+        # but I haven't figured an elegent way to avoid that, so let it filename twice for now
+        emacsclient -a "emacs-daemon.sh $sudo ${params[*]}" -c -n $sudo "${params[@]}"
+    elif [[ "${server_name}" == *"cli_server"* ]]; then
+        emacsclient -a "emacs-daemon.sh $sudo ${params[*]}" -nw -q $sudo "${params[@]}"
     fi
 else
-    if [ "${server_name}" = "gui_server" ]; then
+    if [[ "${server_name}" == *"gui_server"* ]]; then
         # all gui files are sent to the opened editor
-        emacsclient -n "$@"
-    elif [ "${server_name}" = "cli_server" ]; then
+        emacsclient -n $sudo "${params[@]}"
+    elif [[ "${server_name}" == *"cli_server"* ]]; then
         # all cli files are opened with new windows
-        emacsclient -nw "$@"
+        emacsclient -nw $sudo "${params[@]}"
     fi
 fi
 
