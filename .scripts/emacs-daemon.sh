44cd02d2e42ebcd97ba5f71734a63551af495d5d
Allen Pan <aifusenno1@gmail.com>
2021-11-12 03:06:38 -0800

    Emacs scripts support using both socket and tcp previous version is hardcoded to use tcp for gui_server. Now both tcp and socket are supported for any server, although I use socket for everything in my workflow.

--- .scripts/emacs-daemon.sh
+++ .scripts/emacs-daemon.sh
@@ -2,19 +2,17 @@
 
 ### Description
 # Used in emacsclient -a option to start daemon if server not found.
-# This file should only be executed once, at the server creation time
-# Later on, modify the script to use UNIX domain socket for forwarding instead following instruction here: https://blog.goldandapager.io/a-better-emacs-remote-editing-workflow/
+# This file should only be executed once for each server--at the server creation time
 
 # get the server name
 while getopts ":T:f:s:" option; do
     case "${option}" in
         f|s)
             server_name=${OPTARG}
-            server_arg="-${option} ${OPTARG}";;
     esac
 done
 
-if [[ "${server_name}" == *"gui_server"* ]]; then
+if [[ "${server_name}" == *"gui_server" ]]; then
     # load ssh key to avoid entering passphrase when using tramp
     # but only load if key is found. We do not want to type passphrase at emacs
     # startup bascuse we may not use tramp.
@@ -27,18 +25,7 @@ if [[ "${server_name}" == *"gui_server"* ]]; then
     sleep 0.5
     # open a frame
     emacsclient -c -n $@
-
-    # copy server file to all remote server if ssh key is found
-    # otherwise, let ssh do the copy when first time entering passphrase
-    if [[ $ret -eq 0 ]]; then
-        scp ~/.emacs.d/server/gui_server allpan@vm:~/.emacs.d/server 2> /dev/null
-    fi
-
-elif [[ "${server_name}" == *"cli_server"* ]]; then
+elif [[ "${server_name}" == *"cli_server" ]]; then
     emacs --daemon=${server_name}
     emacsclient -nw $@
 fi
-
-
-
-
