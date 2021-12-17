a7d420bbe05bb9a3aa50c93d2610936873de1657
Allen Pan <aifusenno1@gmail.com>
2021-12-17 02:29:06 -0800

    updated comments, added workaround to ssh that removes remote emacs socket before forwarding

--- .scripts/ssh.sh
+++ .scripts/ssh.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 ## Wrapper for ssh
-
 test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh
 
+## Workaround: remove the remote emacs socket
+\ssh vm "rm /tmp/emacs/remote_emacs.sock" &> /dev/null
+
 ssh "$@"
+
