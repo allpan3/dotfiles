489eb6415ef32c922df19d275cc7aaedb0f998c8
Allen Pan <aifusenno1@gmail.com>
2022-01-08 18:56:39 -0800

    rename .scripts/ to scripts

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,7 +4,7 @@
 test -r $HOME/.scripts/ssh-agent-setup.sh && . $HOME/.scripts/ssh-agent-setup.sh
 
 ## Workaround: remove the remote emacs socket
-\ssh vm "rm /tmp/emacs/remote_emacs.sock" &> /dev/null
+# \ssh vm "rm /tmp/emacs/remote_emacs.sock" &> /dev/null
 
 ssh "$@"
 
