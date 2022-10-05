9815809866cee1d7cdb3fb4cdd28d8898e9feb76
Allen Pan <aifusenno1@gmail.com>
2022-10-04 17:01:32 -0700

    temp solution for removing remote socket

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -6,10 +6,9 @@
 test -r $HOME/scripts/ssh-agent-setup.sh && . $HOME/scripts/ssh-agent-setup.sh
 
 ## Workaround: remove the remote emacs socket
-if [ "${@: -1}" == "vm" ]; then
-    \ssh vm "rm /tmp/emacs/remote_emacs.sock" &> /dev/null
+if [ "${@: -1}" == "eecs151" ]; then
+    \ssh vm "rm /tmp/emacs-allpan/remote_emacs.sock" &> /dev/null
 fi
-
 
 ssh $@
 
