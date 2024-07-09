b1e8a4fd685a0e41b6488cc40f83e8c663574e6b
Allen Pan <aifusenno1@gmail.com>
2024-07-09 14:41:11 -0700

    Fix bufferline error due to lazyvim upgrade

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,7 +4,6 @@ return {
 		options = {
 			-- mode = "tab", -- this hides buffers from line, only show tabs
 			separator_style = "slant",
-			always_show_bufferline = false,
 			numbers = function(opts)
 				return string.format("%s·%s", opts.raise(opts.ordinal), opts.lower(opts.id))
 			end,
@@ -32,17 +31,6 @@ return {
 				delay = 150,
 				reveal = { "close" },
 			},
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-			diagnostics = "nvim_lsp",
-			diagnostics_indicator = function(_, _, diag)
-				local icons = require("lazyvim.config").icons.diagnostics
-				local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-					.. (diag.warning and icons.Warn .. diag.warning or "")
-				return vim.trim(ret)
-			end,
 		},
 	},
 	keys = {
@@ -54,5 +42,14 @@ return {
 		{ "<leader>bh", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
 		{ "<leader>bl", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
 		{ "<leader>br", false },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
 	},
 }
