4a437a6ddee107e7f4035bb32b8dce6d079b26b6
Allen Pan <aifusenno1@gmail.com>
2024-07-17 15:55:21 -0700

    Add zenmode plugin

---
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{ "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+	},
+}
