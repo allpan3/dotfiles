d11efd348a37a15a5d943762926506d4355cc3a9
Allen Pan <aifusenno1@gmail.com>
2024-08-15 02:31:07 -0700

    copilot: don't use copilot extra, configure my own setup
    Copilot uses copilot-cmp by default, which doesn't allow accept word. To
    do that I have to not use copilot as a completion source. There're pros
    and cons. Will see how this feels.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.mini-move" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
+		-- { import = "lazyvim.plugins.extras.coding.copilot" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,12 +1,24 @@
 return {
 	"hrsh7th/nvim-cmp",
 	opts = function(_, opts)
-		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 		local cmp = require("cmp")
 
-		-- Toggle complete menu with one key
-		local toggle_complete = function()
-			return function(fallback)
+		local function beginning_of_line()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col == 0 or vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:match("^%s+$") ~= nil
+		end
+
+		local function has_words_before()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
+		opts.mapping = cmp.mapping.preset.insert({
+			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-s>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					if not cmp.close() then
 						fallback()
@@ -20,29 +32,42 @@ return {
 						cmp.complete()
 					end
 				end
-			end
-		end
-
-		opts.mapping = cmp.mapping.preset.insert({
-			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-      -- TODO: may need to revisit this. C-c may conflict in some scenarios
-			["<C-c>"] = toggle_complete(), -- both show and abort; somehow S-space don't work anymore
-			["<Tab>"] = cmp.mapping.confirm({ select = true }), -- accept the current selected item, otherwise have to press down to select the first item
-			["<S-CR>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
-			}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			["<C-CR>"] = function(fallback)
-				cmp.abort()
-				fallback()
-			end,
+			end, { "i", "s" }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				-- cmp menu given the highest priority
+				if cmp.visible() then
+					cmp.confirm({ select = true }) -- accept the current selected item, otherwise have to press down to select the first item
+				elseif require("copilot.suggestion").is_visible() and not beginning_of_line() then
+					require("copilot.suggestion").accept()
+				-- elseif luasnip.expandable() then
+				--   luasnip.expand()
+				-- elseif vim.snippet.active({ direction = 1 }) then
+				-- 	vim.schedule(function()
+				-- 		vim.snippet.jump(1)
+				-- 	end)
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			-- 		["<S-CR>"] = cmp.confirm({
+			-- 			behavior = cmp.ConfirmBehavior.Replace,
+			-- 			select = false,
+			-- 		}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			-- 		["<C-CR>"] = function(fallback)
+			-- 			cmp.abort()
+			-- 			fallback()
+			-- 		end,
 		})
 
-    -- opts.cmdline(':', {
-    --   mapping = cmp.mapping.preset.insert()
-    -- })
-	end
+		-- TODO: still don't know how to configure both insert and cmdline mapping
+
+		opts.experimental = {
+			ghost_text = false, -- this feature conflict with copilot.vim's preview.
+			-- ghost_text = {
+			-- 	hl_group = "CmpGhostText",
+			-- },
+		}
+	end,
 }
