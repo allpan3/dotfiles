cb14b599a4e616d41d46eaf2c1f5f043afd56845
Allen Pan <aifusenno1@gmail.com>
2024-08-21 12:37:57 -0700

    neovim: change copilot and nvim-cmp keymapping
    nvim-cmp now doesn't automatically select the first entry. This allows
    better compatibility with copliot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,21 +3,10 @@ return {
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
+
 		require("copilot").setup({
 			panel = {
-				enabled = true,
-				auto_refresh = false,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-				layout = {
-					position = "bottom", -- | top | left | right
-					ratio = 0.4,
-				},
+				enabled = false,
 			},
 			suggestion = {
 				enabled = true,
@@ -25,9 +14,9 @@ return {
 				hide_during_completion = true,
 				debounce = 75,
 				keymap = {
-					accept = false,
-					accept_word = "<M-l>",
-					accept_line = "<M-j>",
+					accept = "<C-i>",
+					accept_word = "<C-m>",
+					accept_line = false,
 					next = "<M-]>",
 					prev = "<M-[>",
 					dismiss = "<C-]>",
@@ -47,5 +36,6 @@ return {
 			copilot_node_command = "node", -- Node.js version must be > 18.x
 			server_opts_overrides = {},
 		})
+
 	end,
 }
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,40 +13,44 @@ return {
 			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
 
+    opts.auto_select = false
+    opts.completion = {
+      completeopt = "menu,menuone,noinsert,noselect"
+    }
+		opts.preselect = cmp.PreselectMode.None
+		opts.window = {
+			-- Make the completion menu bordered.
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		}
 		opts.mapping = cmp.mapping.preset.insert({
 			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      -- this sometimes doesn't work for the first time; signature help took precedence over this, but it works the second menu is opened
 			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-s>"] = cmp.mapping(function(fallback)
+      -- toggle menu
+			["<C-s>"] = cmp.mapping(function()
 				if cmp.visible() then
-					if not cmp.close() then
-						fallback()
-					else
-						cmp.close()
-					end
+					cmp.close()
 				else
-					if not cmp.complete() then
-						fallback()
-					else
-						cmp.complete()
-					end
+          cmp.mapping.complete()
 				end
 			end, { "i", "s" }),
 			["<Tab>"] = cmp.mapping(function(fallback)
-				-- cmp menu given the highest priority
-				if cmp.visible() then
-					cmp.confirm({ select = true }) -- accept the current selected item, otherwise have to press down to select the first item
-				elseif require("copilot.suggestion").is_visible() and not beginning_of_line() then
-					require("copilot.suggestion").accept()
+				if cmp.visible() and cmp.get_active_entry() then
+					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }) 
+          -- select = true accepts the current selected item, otherwise have to press down to select the first item
+        elseif require("copilot.suggestion").is_visible() and not beginning_of_line() then
+					require("copilot.suggestion").accept_line()
 				-- elseif luasnip.expandable() then
 				--   luasnip.expand()
-				-- elseif vim.snippet.active({ direction = 1 }) then
-				-- 	vim.schedule(function()
-				-- 		vim.snippet.jump(1)
-				-- 	end)
+				elseif vim.snippet.active({ direction = 1 }) then
+					vim.schedule(function()
+						vim.snippet.jump(1)
+					end)
 				elseif has_words_before() then
-					cmp.complete()
+					cmp.mapping.complete()
 				else
 					fallback()
 				end
