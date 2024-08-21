d1a6138afdf1c99cba104417c74ee5f82565a9d9
Allen Pan <aifusenno1@gmail.com>
2024-08-20 18:30:32 -0700

    neovim: add python debugger config, not sure if autocmds is the right place

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,2 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+
+local home = os.getenv('HOME')
+vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match('(%S+)%s+%(.-%)')
+vim.g.python3_host_prog = home .. "/.pyenv/shims/python3"
