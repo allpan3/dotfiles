68ae6b9985afda3a86f81b17793d84163ff77265
Allen Pan <aifusenno1@gmail.com>
2024-08-26 01:04:59 -0700

    neovim: my carbonfox customization

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,47 @@
 return {
 	{
 		"EdenEast/nightfox.nvim",
-		lazy = true,
-    -- themes:
-    --   nightfox
-    --   dayfox
-    --   carbonfox
+		-- themes:
+		--   nightfox
+		--   dayfox
+		--   carbonfox
+		opts = {
+			specs = {
+				carbonfox = {
+					syntax = {
+						-- conditional = "magenta.bright",
+						-- keyword = "magenta.base",
+					},
+				},
+			},
+			groups = {
+				carbonfox = {
+					WinSeparator = { fg = "palette.bg3" },
+					FloatBorder = { fg = "palette.bg4" },
+					MatchParen = { fg = "palette.pink", style = "bold,underline" }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+					-- Visual = { bg = spec.sel0 }, -- Visual mode selection (inverse won't work)
+				},
+			},
+			palettes = {
+				carbonfox = {
+					comment = "#808892",
+					sel0 = "#353535", -- visual
+				},
+			},
+		},
 	},
 	{
 		"allpan3/oxocarbon.nvim",
-    lazy = true,
-    -- themes:
-    --   oxocarbon
+		-- I made the comment more readable
+		-- Borderless
+		-- flash label not very easy to see
+		-- themes:
+		--   oxocarbon
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-      colorscheme = "oxocarbon",
-			-- colorscheme = "carbonfox",
+			colorscheme = "carbonfox",
 			-- colorscheme = "catppuccin",
 		},
 	},
