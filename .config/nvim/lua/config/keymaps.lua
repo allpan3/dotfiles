f4a7513681832fbd31cf082c41df7ecca74f0bb7
Allen Pan <aifusenno1@gmail.com>
2024-08-26 01:09:10 -0700

    neovim: multiple keymap updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- LazyVim default keymaps are automatically loaded: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
+---------- Keys free to map -------------
+-- key - mode - description
+-- M - n - not useful
+-----------------------------------------
+
 -- remap: recursively map the keys when the rhs contains lhs
 -- silent: when mapping a key to a command, don't show the command prompt popup
 local function map(mode, lhs, rhs, opts)
@@ -23,12 +28,17 @@ end
 -- TODO: This function doesn't support icon, not sure which function I should use
 vim.keymap.set("n", "<leader><space>", ":", { desc = "Command Mode" })
 -- Edit
-vim.keymap.del({"n", "t"}, "<C-_>") -- neovim default is terminal
-map({ "n", "i", "v", "x" }, "<C-_>", "<cmd>undo<CR>", { desc = "Undo"}) -- same as terminal
-map({ "n", "i", "v", "x" }, "<M-r>", "<cmd>redo<CR>", { desc = "Redo"}) -- ctrl-r is search history in shell; this is mainly for mapping cmd-shift-z
-map("n", "U", "<cmd>redo<CR>", { desc = "Redo"}) -- pair with u as undo
-map("n", "<S-Enter>", "o<Esc>") -- shift-enter to insert new line below in normal mode
+vim.keymap.del({ "n", "t" }, "<C-_>") -- neovim default is terminal
+map({ "n", "i", "v", "x" }, "<C-_>", "<cmd>undo<CR>", { desc = "Undo" }) -- same as terminal
+map({ "n", "i", "v", "x" }, "<M-r>", "<cmd>redo<CR>", { desc = "Redo" }) -- ctrl-r is search history in shell; this is mainly for mapping cmd-shift-z
+map("n", "U", "<C-r>", { desc = "Redo" }) -- pair with u as undo; using ctrl-r instead of :redo allows count
+map("n", "<S-enter>", "o<Esc>") -- shift-enter to insert new line below in normal mode
 map("i", "<M-e>", "<C-o>d$") -- delete to end of line, match shell (customized)
+-- Indentation
+map("n", "<tab>", ">>", { desc = "Indent" })
+map("n", "<S-tab>", "<<", { desc = "Unindent" })
+map("v", "<tab>", ">", { desc = "Indent" })
+map("v", "<S-tab>", "<", { desc = "Unindent" })
 
 -- Tab management
 -- LazyVim default uses <tab> as secondary key. I prefer to use t
@@ -81,13 +91,16 @@ map({ "n", "x", "s" }, "<leader>fs", "<cmd>w<CR>", { desc = "Save File" })
 vim.keymap.set({ "n", "x", "s" }, "<leader>fS", ":w ", { desc = "Save as" })
 
 -- Clear Highlight
--- Disable LazyVim default hlsearch: don't want escaspe to clear highlight, use <leader>ur
-vim.keymap.del({ "i", "n" }, "<esc>")
+-- LazyVim confiugres <esc> to clear highlight in both insert and normal mode.
+-- I prefer to keep it when I exit from insert to normal, but only clear when press <esc> in normal mode
+vim.keymap.del({ "n" }, "<esc>")
 
 -- Navigation
 map("n", "<M-f>", "e") -- word forward, match terminal; <M-b> is word backward by default
 map({ "n", "v", "x", "o" }, "gh", "^", { desc = "Goto Beginning of Indented Line" })
-map({ "n", "v", "x", "o" }, "gl", "$", { desc = "Goto End of Line" })
+-- in visual mode $ includes the new line char, which isn't consistent with other operator mode
+-- using g_ excludes the new line char, better consistency
+map({ "n", "v", "x", "o" }, "gl", "g_", { desc = "Goto End of Line" })
 map("i", "<M-b>", "<S-left>") -- word backward, match shell (shift-left is neovim default)
 map("i", "<M-f>", "<S-right>") -- word forward, match shell (shift-right is neovim default)
 
@@ -113,17 +126,19 @@ LazyVim.toggle.map(
 
 -- Colorscheme picker
 map("n", "<leader>uc", LazyVim.pick("colorscheme", { enable_preview = true }), { desc = "Colorscheme" })
-require("which-key").add({"<leader>uc", icon = {icon = "", color = "blue"}, desc = "Colorscheme"})
+require("which-key").add({ "<leader>uc", icon = { icon = "", color = "blue" }, desc = "Colorscheme" })
 
--- By default, pasting in visual mode puts the replaced text in register. This keeps the old text
-map("v", "p", '"_dP')
+-- By default, pasting in visual mode puts the replaced text in default register. This writes to blackhole register instead
+-- both p and P work
+map({ "v", "x" }, "p", '"_dP')
 
 -- Floating terminal
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
 
 -- Copy current file path
-map("n", "<leader>pf", "<cmd>let @+ = expand('%:p')<cr>", { desc = "Copy File Path" })
+map("n", "<leader>fy", "<cmd>let @+ = expand('%:p')<cr>", { desc = "Copy File Path" })
+vim.keymap.set("n", "<leader>fp", ":edit ", { desc = "Open Path" }) -- open file with absolute or relative path
 
 -- LazyVim menu
 vim.keymap.del("n", "<leader>l")
