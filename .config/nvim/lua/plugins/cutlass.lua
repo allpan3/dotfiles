7c2a9bfad28dcf3d95382b048dbcc7aa6d4eda28
Allen Pan <aifusenno1@gmail.com>
2024-08-29 15:17:34 -0700

    neovim: drop named register in cutlass - see in-code comments

--- .config/nvim/lua/plugins/cutlass.lua
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -5,9 +5,13 @@ return {
 		cut_key = "x",
 		override_del = true,  -- <del> puts in blackhole register
 		exclude = {"s"},
-		registers = {
-			delete = "d",  -- puts in "d register
-			change = "c",  -- puts in "c register
-		},
+    -- Putting daleted/changed text in a named register will cause " register to still be written. When we
+    -- set clipboard to + register by default, we won't use " so things are fine (+ isn't affected by named register).
+    -- However, There's some issue with OSC 52 that forced us to paste from the " register instead of the + register in the remote machine.
+    -- This forces us to always write to black hole register in order to keep " untouched, so we cannot use a named register here, at least for now.
+		-- registers = {
+		-- 	delete = "d",  -- puts in "d register
+		-- 	change = "c",  -- puts in "c register
+		-- },
 	},
 }
