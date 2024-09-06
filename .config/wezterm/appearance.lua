a6cd926b177fe808df6b2829bd1ae354264ff027
Allen Pan <aifusenno1@gmail.com>
2024-09-05 23:48:10 -0700

    wezterm: update theme and tab style

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -1,41 +1,50 @@
 local wezterm = require("wezterm")
-local scheme = wezterm.get_builtin_color_schemes()["Dark+"]
+local scheme = wezterm.get_builtin_color_schemes()["carbonfox"]
 
 ----------------
 -- Appearance --
 ----------------
 return {
-	color_scheme = "Dark+",
+	color_scheme = "carbonfox",
 	-- font = wezterm.font("MesloLGS Nerd Font Mono"),
 	font = wezterm.font("JetBrainsMono Nerd Font"),
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	font_size = 14,
-  line_height = 0.85,
-  cell_width = 1.0,
-  adjust_window_size_when_changing_font_size = false,
+	line_height = 0.85,
+	cell_width = 1.0,
+	adjust_window_size_when_changing_font_size = false,
 
 	-- Tab bar
-	use_fancy_tab_bar = false,
+	-- use_fancy_tab_bar = false,
 	-- config.enable_tab_bar = false
 	hide_tab_bar_if_only_one_tab = true,
 	-- config.tab_bar_at_bottom = true
 	tab_max_width = 25,
 
 	-- Fancy tab bar appearance
-	window_frame = { font_size = 12 },
+	window_frame = {
+		font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Bold" }),
+		-- Controls the size of tar bar
+		font_size = 12,
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#1e1e1e",
 
-	-- Retro tab bar appearance
-	-- colors = {
-	-- 	tab_bar = {
-	-- 		background = scheme.background,
-	-- 		new_tab = { bg_color = "#2e3440", fg_color = scheme.ansi[8], intensity = "Bold" },
-	-- 		new_tab_hover = { bg_color = scheme.ansi[1], fg_color = scheme.brights[8], intensity = "Bold" },
-	-- 		-- format-tab-title
-	-- 		-- active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
-	-- 		-- inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
-	-- 		-- inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
-	-- 	},
-	-- },
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#1e1e1e",
+	},
+
+	colors = {
+		tab_bar = {
+			inactive_tab = { bg_color = scheme.background, fg_color = "#808080" },
+			-- The color of the inactive tab bar edge/divider
+			inactive_tab_edge = "#808080",
+		},
+	},
+
+	-- Hide window bar
+	window_decorations = "RESIZE",
 
 	-- The amount of padding between the window border and the terminal cells.
 	window_padding = {
@@ -45,6 +54,7 @@ return {
 		right = 0,
 	},
 
+  -- Inactive pane dimming
 	inactive_pane_hsb = {
 		saturation = 0.9,
 		brightness = 0.9,
