cd6e01120b317f8c7c81ac42f04210d927a90af5
Allen Pan <aifusenno1@gmail.com>
2025-01-15 14:06:34 -0800

    neovim: remove obsolete which-key groups

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,11 +2,9 @@ return {
 	"folke/which-key.nvim",
 	opts = {
 		spec = {
-			{ "<leader>t", group = "tabs" },
 			{ "<leader><tab>", desc = "Switch to Other Buffer" }, -- Overrides LazyVim default
 			{ "<leader>l", group = "lazy" },
 			{ "<leader><space>", icon = "" },
-			{ "<leader>z", icon = { icon = "󱅻", color = "cyan" }, desc = "Zen" },
 		},
 	},
 }
