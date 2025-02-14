b7822b7e97fb39e9a0707c9fcc7a32f9c3114e69
Allen Pan <aifusenno1@gmail.com>
2025-02-13 17:46:20 -0800

    nvim: snacks.picker and snacks explorer keymaps

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -24,5 +24,57 @@ return {
 				-- { pane = 2, section = "terminal", cmd = "echo ' '", padding = 1 },
 			},
 		},
+
+    -- Don't know what this does
+    statuscolumn = {
+      enabled = true
+    },
+
+		picker = {},
+
+		explorer = {
+			-- replace_netrw = true, -- Replace netrw with the snacks explorer
+		},
+	},
+
+	keys = {
+		{ "<leader><space>", false }, -- set to command mode
+		-- Explorer
+		{ "<leader>e", false },
+		{ "<leader>E", false },
+		{ "<leader>fe", false },
+		{ "<leader>fE", false },
+		{
+			"<leader>ee",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		{
+			"<leader>ee",
+			function()
+				Snacks.explorer({ cwd = LazyVim.root() })
+			end,
+			desc = "Explorer (root)",
+		},
+		{
+			"<leader>eE",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Explorer (cwd)",
+		},
+
+		-- Picker
+    -- git (some are set in keymaps.lua and gitsigns.lua)
+    -- This shows the commit histories that contain this line, but shows all changes in the commit instead of just the hunk containing the line
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line"},
+    { "<leader>gl", function() Snacks.picker.git_log({ cwd = LazyVim.root.git() }) end, desc = "Git Log (root)" },
+    -- { "<leader>gL", function() Snacks.picker.git_log() end, desc = "Git Log (cwd)" }, -- Does this work?
+    { "<leader>gc", false },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+		{ "<leader>gh", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gD", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
 	},
 }
