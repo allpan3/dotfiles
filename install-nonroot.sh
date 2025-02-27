1ae52a43500ed29993da227ee3860b7273f31b28
Allen Pan <aifusenno1@gmail.com>
2025-02-26 21:06:56 -0800

    Add install script

---
+++ install-nonroot.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env bash
+#************************
+# Modify these variables
+#************************
+PREFIX=${HOME}/.local
+CONDA_HOME=${HOME}/.conda
+
+##### Set up dotfiles #####
+git clone --separate-git-dir=$HOME/.dotfiles.git https://github.com/allpan3/dotfiles.git dotfiles-tmp
+rsync --recursive --verbose dotfiles-tmp/ $HOME/
+rm -rf dotfiles-tmp
+git checkout nonroot
+git config status.showUntrackedFiles no
+# Refresh
+source .bashrc
+
+##### Install package managers #####
+# conda
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b -p $CONDA_HOME
+rm -f Miniforge3-$(uname)-$(uname -m).sh
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+
+# ble.sh
+mkdir ${PREFIX}/source
+pushd ${PREFIX}/source
+git clone --recursive https://github.com/akinomyoga/ble.sh.git
+cd ble.sh
+make && make install
+popd
+
+# Refresh
+source .bashrc
+
+##### Install software #####
+# neovim
+conda install nvim nodejs ripgrep
+
+# Zellij
+cargo install --locked zellij
+
+# Other utilities
+conda install fzf fd-find bat zoxide dnachun::btop
+
+# Git
+conda install git gh git-delta lazygit
+
+# C/C++ development
+conda install bear
+
+# GPU
+conda install nvtop
+
+# Refresh
+source .bashrc
+
+
