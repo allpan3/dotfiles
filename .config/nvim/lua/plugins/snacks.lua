afd649203b001791d1477cdc156eca1aab937281
Allen Pan <allpan@stanford.edu>
2025-03-08 22:36:00 -0800

    nvim: lower case keymaps for cwd

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -50,18 +50,16 @@ return {
 						--       support CSI u, and better to match fzf and snacks picker
 						["<c-n>"] = { "history_forward", mode = { "i", "n" } },
 						["<c-p>"] = { "history_back", mode = { "i", "n" } },
-						-- Right now there's no action to directly toggle preview wrap, have to switch to preview window then <leader>uw
-						["<c-m>"] = { "cycle_win", mode = { "i", "n" } },
             -- With zellij kitty keyboard protocol disabled, <c-/> is the same as <c-_> in neovim, which is reserved by undo
 						-- ["<c-/>"] = { "toggle_preview", mode = { "i", "n" } },
 						["<c-t>"] = { "trouble_open", mode = { "n", "i" } },
+            -- Right now there's no action to directly toggle preview wrap, have to switch to preview window then <leader>uw
 					},
 				},
 				-- result list window
 				-- INFO: once enter list or preview window, can use all normal mode keybings (including leader keys)
 				list = {
 					keys = {
-						["<c-m>"] = "cycle_win",
 						-- ["<c-/>"] = "toggle_preview",
 						["<c-t>"] = "trouble_open",
 					},
@@ -72,7 +70,6 @@ return {
 				-- preview window
 				preview = {
 					keys = {
-						["<c-m>"] = "cycle_win",
 						-- ["<c-/>"] = "toggle_preview",
 					},
 				},
@@ -85,40 +82,37 @@ return {
 		},
 
 		explorer = {
-			replace_netrw = false, -- Replace netrw with the snacks explorer
+			-- replace_netrw = false, -- Replace netrw with the snacks explorer
 		},
 	},
 
+	-- stylua: ignore
 	keys = {
 		{ "<leader><space>", false }, -- set to command mode
+		{ "<leader>,", false }, -- leave for something else
+    { "<leader>*", LazyVim.pick("grep_word"), desc = "Grep Word (root) (", mode = { "n", "x" } },
+
 		-- Explorer
 		{ "<leader>e", false },
 		{ "<leader>E", false },
 		{ "<leader>fe", false },
 		{ "<leader>fE", false },
-		{
-			"<leader>ee",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "File Explorer",
-		},
-		{
-			"<leader>ee",
-			function()
-				Snacks.explorer({ cwd = LazyVim.root() })
-			end,
-			desc = "Explorer (root)",
-		},
-		{
-			"<leader>eE",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "Explorer (cwd)",
-		},
+		{ "<leader>ee", function() Snacks.explorer() end, desc = "File Explorer", },
+		{ "<leader>ee", function() Snacks.explorer({ cwd = LazyVim.root() }) end, desc = "Explorer (root)", },
+		{ "<leader>eE", function() Snacks.explorer() end, desc = "Explorer (cwd)", },
 
 		-- Picker
+    -- files
+    -- use <M-c> to toggle between cwd and root
+		{ "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (root)" },
+    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (cwd)" },
+    -- grep
+    { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (root)" },
+    { "<leader>sw", LazyVim.pick("grep_word", { root = false }), desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
+    { "<leader>sW", LazyVim.pick("grep_word"), desc = "Visual selection or word (root)", mode = { "n", "x" } },
+
+   
 		-- git (some are set in keymaps.lua and gitsigns.lua)
 		-- This shows the commit histories that contain this line, but shows all changes in the commit instead of just the hunk containing the line
 		{
