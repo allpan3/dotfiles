4e3df7042fe4d1df172ff81e7f7771e183d6b188
Allen Pan <aifusenno1@gmail.com>
2025-04-23 23:56:39 -0700

    nvim: add nushell extra

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.nushell" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
